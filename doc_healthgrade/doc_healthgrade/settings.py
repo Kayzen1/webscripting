1280db183c74be22609dd24c79fc4176455fa672
Zeng Yuting <kay@Zengs-MacBook-Pro.local>
2023-02-25 21:18:22 -0600

    change to dynamic website crawling

--- doc_healthgrade/doc_healthgrade/settings.py
+++ doc_healthgrade/doc_healthgrade/settings.py
@@ -15,10 +15,11 @@ NEWSPIDER_MODULE = 'doc_healthgrade.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'doc_healthgrade (+http://www.yourdomain.com)'
-
+USER_AGENT = 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.86 Mobile Safari/537.36 (compatible; Googlebot/2.1; http://www.google.com/bot.html)'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+PROCY_POOL_ENABLED = True #使用不一样的ip地址
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -86,3 +87,8 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
---
+++ doc_healthgrade/doc_healthgrade/spiders/health_spider.py
@@ -0,0 +1,81 @@
+import scrapy
+from scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+
+class HealthgradesSpider(scrapy.Spider):
+    name = 'doctor'
+    allowed_domains = ['www.healthgrades.com']
+    start_urls = ['https://www.healthgrades.com/usearch?what=Dentistry&pageNum={}&sort.provider=bestmatch'.format(i) for i in range(1, 59)]
+
+    def __init__(self):
+        super().__init__()
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(options=options)
+
+    def parse(self, response):
+        self.driver.get(response.url)
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '.card-name a'))
+            )
+            sel = Selector(text=self.driver.page_source)
+            provider_links = sel.css('.card-name a::attr(href)').getall()
+            for provider_link in provider_links:
+                provider_url = response.urljoin(provider_link)
+                yield scrapy.Request(url=provider_url, callback=self.parse_provider_page)
+        finally:
+            self.driver.quit()
+
+    def parse_provider_page(self, response):
+        sel = Selector(text=response.body)
+        doctor_name = sel.css('h1[data-qa-target="ProviderDisplayName"]::text').get()
+        speciality = sel.css('span[data-qa-target="ProviderDisplaySpeciality"]::text').get()
+        gender = sel.css('span[data-qa-target="ProviderDisplayGender"]::text').get(default='')
+        if gender is not None:
+            gender = gender.strip()
+        else:
+            gender = ''
+
+        # comments = sel.css('.comments__commentText::text').getall()
+        # comments = ' '.join([comment.strip() for comment in comments])
+        # speciality = sel.css('.v2-sp-2::text').get()
+        yield {
+            'Doctor_Name': doctor_name,
+            'Gender': gender,
+            'Speciality': speciality
+            # 'Comments': comments
+        }
+
+    def closed(self, reason):
+        spider_closed = getattr(self, 'spider_closed', None)
+        if callable(spider_closed):
+            spider_closed(self)
+        self.driver.quit()
+
+    def spider_closed(self, spider):
+        item_count = self.crawler.stats.get_value('item_scraped_count')
+        if item_count > 0:
+            with open('output.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['Doctor_Name', 'Gender', 'Speciality'])
+                for i in range(1, item_count + 1):
+                    item = self.export_csv(i)
+                    if item is not None:
+                        writer.writerows(item)
+
+
+    def export_csv(self, index):
+        with open('output.csv', mode='r') as file:
+            reader = csv.reader(file)
+            for row_num, row in enumerate(reader, start=1):
+                if row_num == index:
+                    return row
+        return None
+
